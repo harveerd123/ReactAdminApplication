a767599d148ad5df96eaf7440b8ce3a8da7258ff
harveerd123 <106449958+harveerd123@users.noreply.github.com>
2025-03-19 19:21:40 +0000

    Adding server side code done using .NET
    Server side code of application using .NET, EF & SQLite

---
+++ ReactAdminApplication.Server/Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactAdminApplication.Server.Models;
+
+namespace ReactAdminApplication.Server.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; } 
+    }
+}
---
+++ ReactAdminApplication.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactAdminApplication.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Email {  get; set; }
+
+        public string Password { get; set; }
+    }
+}
